5b1e047ac85678731d471303026c218f8d77066b
Lincoln Martinez <lincoln.martinez@tecsup.edu.pe>
2021-04-06 18:47:08 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio03_1/Ejercicio03_1/ImageResourceExtension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Reflection;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Ejercicio03_1
+{
+    [Preserve(AllMembers = true)]
+    [ContentProperty(nameof(Source))]
+    class ImageResourceExtension : IMarkupExtension
+    {
+        public String Source { get; set; }
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+            var imageSource = ImageSource.FromResource(Source,
+                    typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+            return imageSource;
+        }
+    }
+}
